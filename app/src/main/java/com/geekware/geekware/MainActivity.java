20010137f1af875ac2046846a7dababfcb24a8e0
Abhinav Tripathi <genuineabhinav@gmail.com>
2015-12-10 18:53:32 +0530

    Created the repo with basic project.

---
+++ app/src/main/java/com/geekware/geekware/MainActivity.java
@@ -0,0 +1,48 @@
+package com.geekware.geekware;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Always follow the conventions as used in this file.
+ *   -- Always make all the GUI variables in the class outside any function then initialise them in
+ *      the onCreate function. Follow the naming convention too.
+ *   -- Try to keep everything aligned.
+ *   -- Always first assign all variables then only start other code as action listeners.
+ *   -- Always name activity classes as *Activity
+ */
+
+public class MainActivity extends ActionBarActivity {
+    Button btnSignUp, btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignUp = (Button) findViewById(R.id.btnSignUp);
+        btnLogin  = (Button) findViewById(R.id.btnLogin);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+}
