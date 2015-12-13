abd428cf37a5c5dcefd5e4ed3b6c60ebab934aa9
Abhinav Tripathi <genuineabhinav@gmail.com>
2015-12-13 19:50:59 +0530

    Filled the parse app keys.

--- app/src/main/java/com/geekware/geekware/GeekwareApplication.java
+++ app/src/main/java/com/geekware/geekware/GeekwareApplication.java
@@ -6,12 +6,11 @@ import com.parse.Parse;
 
 /**
  * This class will initialise the parse APIs
- * It is created once by android OS when our app is launched.
+ * It is instantiated once by android OS when our app is launched.
  */
 public class GeekwareApplication extends Application {
-    //TODO: these will be filled after creating the app on parse.com
-    private static final String APP_ID = "";
-    private static final String CLIENT_KEY = "";
+    private static final String APP_ID = "1i9zwapMoXmMkxByhj0XRGX8plvfWmGpf0VvxfFf";
+    private static final String CLIENT_KEY = "mmLshv8ixM0uYKloYj1Q7SWcfIP9PRequw0aMzZv";
 
     @Override
     public void onCreate() {
